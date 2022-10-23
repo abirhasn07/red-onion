1f7277804e0b9cd4236aa904ae2cf4c82aaf5111
abirhasn07 <iamabirhasan07@gmail.com>
2022-10-23 14:39:34 +0600

    image kit update

--- src/database/data.js
+++ src/database/data.js
@@ -5,126 +5,126 @@ const food1 = {
     id: 1,
     price: 23.50,
     categories: "lunch",
-    img: "https://ik.imagekit.io/abirhasan/lunch1_4rfCWT3vT.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121035892"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/lunch1_4rfCWT3vT.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121035892"
 }
 const food2 = {
     name: "Fried Chicken Bento",
     id: 2,
     price: 9.99,
     categories: "lunch",
-    img: "https://ik.imagekit.io/abirhasan/lunch2__rKsqs382.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121039430"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/lunch2__rKsqs382.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121039430"
 }
 const food3 = {
     name: "Tarragon Rubbed Salmon",
     id: 3,
     price: 6.99,
     categories: "lunch",
-    img: "https://ik.imagekit.io/abirhasan/lunch5_TAEF_aAlI.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121035669"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/lunch5_TAEF_aAlI.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121035669"
 }
 const food4 = {
     name: "Indian Lunch",
     id: 4,
     price: 8.99,
     categories: "lunch",
-    img: "https://ik.imagekit.io/abirhasan/lunch6_iTX207Yk2.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121034946"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/lunch6_iTX207Yk2.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121034946"
 }
 const food5 = {
     name: "Beef Steak",
     id: 5,
     price: 15.99,
     categories: "lunch",
-    img: "https://ik.imagekit.io/abirhasan/lunch4_rFmTO_UIp.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121034771"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/lunch4_rFmTO_UIp.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121034771"
 }
 const food6 = {
     name: "Honey Soy Salmon ",
     id: 6,
     price: 17.50,
     categories: "lunch",
-    img: "https://ik.imagekit.io/abirhasan/lunch3_dMbYgC9o_.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121031932"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/lunch3_dMbYgC9o_.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121031932"
 }
 const food7 = {
     name: "Classic Burger ",
     id: 7,
     price: 12.50,
     categories: "dinner",
-    img: "https://ik.imagekit.io/abirhasan/dinner2_0_3RNLcwP.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121029110"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/dinner2_0_3RNLcwP.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121029110"
 }
 const food8 = {
     name: "Honey Meat ",
     id: 8,
     price: 11.50,
     categories: "dinner",
-    img: "https://ik.imagekit.io/abirhasan/dinner1_Q9_kTPIns.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121028333"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/dinner1_Q9_kTPIns.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121028333"
 }
 const food9 = {
     name: "Creme Noodles",
     id: 9,
     price: 7.50,
     categories: "dinner",
-    img: "https://ik.imagekit.io/abirhasan/breakfast4_-0RqCtGjd.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121026046"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/breakfast4_-0RqCtGjd.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121026046"
 }
 const food10 = {
     name: "Indian Burger",
     id: 10,
     price: 1.50,
     categories: "dinner",
-    img: "https://ik.imagekit.io/abirhasan/dinner4_8QTJXeIVD.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121026988"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/dinner4_8QTJXeIVD.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121026988"
 }
 const food11 = {
     name: "Pork Sandwich",
     id: 11,
     price: 40.00,
     categories: "dinner",
-    img: "https://ik.imagekit.io/abirhasan/breakfast1_PEAZ7PrsW.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121025091"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/breakfast1_PEAZ7PrsW.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121025091"
 }
 const food12 = {
     name: "Chicken Roast",
     id: 12,
     price: 8.50,
     categories: "dinner",
-    img: "https://ik.imagekit.io/abirhasan/breakfast5_RJQHTgxA1.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121025347"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/breakfast5_RJQHTgxA1.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121025347"
 }
 const food13 = {
     name: "Easter Delight",
     id: 13,
     price: 10.50,
     categories: "breakfast",
-    img: "https://ik.imagekit.io/abirhasan/dinner5_n8xh589qP.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121024386"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/dinner5_n8xh589qP.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121024386"
 }
 const food14 = {
     name: "Easter Delight",
     id: 14,
     price: 7.50,
     categories: "breakfast",
-    img: "https://ik.imagekit.io/abirhasan/breakfast6_fe-NqBIaVt.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121024193"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/breakfast6_fe-NqBIaVt.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121024193"
 }
 const food15 = {
     name: "Easter Delight",
     id: 15,
     price: 3.50,
     categories: "breakfast",
-    img: "https://ik.imagekit.io/abirhasan/dinner3_E9t5vTdJ8.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121023923"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/dinner3_E9t5vTdJ8.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121023923"
 }
 const food16 = {
     name: "Easter Delight",
     id: 16,
     price: 5.50,
     categories: "breakfast",
-    img: "https://ik.imagekit.io/abirhasan/breakfast2_TxXgXSGM9.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121023550"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/breakfast2_TxXgXSGM9.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121023550"
 }
 const food17 = {
     name: "SAKE TEMAKI",
     id: 17,
     price: 4.99,
     categories: "breakfast",
-    img: "https://ik.imagekit.io/abirhasan/breakfast3_XykfAWnBU.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121021736"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/breakfast3_XykfAWnBU.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121021736"
 }
 const food18 = {
     name: "Tiener Schnitze",
     id: 18,
     price: 10.50,
     categories: "breakfast",
-    img: "https://ik.imagekit.io/abirhasan/dinner6_0znsLL1m7.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121015649"
+    img: "https://ik.imagekit.io/abirhasan/tr:w-300/dinner6_0znsLL1m7.png?ik-sdk-version=javascript-1.4.3&updatedAt=1666121015649"
 }
 
 // const categories = ["breakfast", "lunch", "dinner"]
